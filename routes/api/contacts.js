3ed20e4c09724ef40fbfd11374141afbc87d0e39
SMAILe3001 <smaile3001@gmail.com>
2023-07-14 18:42:29 +0300

    Методи і запитами
    Додав методи для роботи з файлом
    Змінив назву роута
    Змінив запит для отримання всіх даних
    Видалив помилково додану папку db
    Оновив пакети

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,28 +1,28 @@
 import express from 'express';
 
-// import contactsPath from '../../db/contacts.js';
+import contactsServise from '../../models/contacts.js';
 
-const router = express.Router();
+const contactsRouter = express.Router();
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' });
-  // res.send(contactsPath);
+contactsRouter.get('/', async (req, res, next) => {
+  const data = await contactsServise.listContacts();
+  res.json(data);
 });
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' });
-});
-
-router.post('/', async (req, res, next) => {
+contactsRouter.get('/:contactId', async (req, res, next) => {
   res.json({ message: 'template message' });
 });
 
-router.delete('/:contactId', async (req, res, next) => {
+contactsRouter.post('/', async (req, res, next) => {
   res.json({ message: 'template message' });
 });
 
-router.put('/:contactId', async (req, res, next) => {
+contactsRouter.delete('/:contactId', async (req, res, next) => {
   res.json({ message: 'template message' });
 });
 
-export default router;
+contactsRouter.put('/:contactId', async (req, res, next) => {
+  res.json({ message: 'template message' });
+});
+
+export default contactsRouter;
