814c548e8cdd0b1394ac11a4a4bbe595da3a799f
SMAILe3001 <smaile3001@gmail.com>
2023-07-14 19:33:38 +0300

    getContactId, error
    Зробив запит контакта
    Додав обробник помилок

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,16 +1,30 @@
 import express from 'express';
 
 import contactsServise from '../../models/contacts.js';
+import { HttpError } from '../../utils/HttpError.js';
 
 const contactsRouter = express.Router();
 
 contactsRouter.get('/', async (req, res, next) => {
-  const data = await contactsServise.listContacts();
-  res.json(data);
+  try {
+    const result = await contactsServise.listContacts();
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
 });
 
 contactsRouter.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' });
+  try {
+    const { contactId } = req.params;
+    const result = await contactsServise.getContactById(contactId);
+    if (!result) {
+      throw HttpError(404, 'Not found');
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
 });
 
 contactsRouter.post('/', async (req, res, next) => {
