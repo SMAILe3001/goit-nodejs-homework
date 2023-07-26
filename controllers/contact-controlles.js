40f268c57556b061d2347c646d480035a965b59e
SMAILe3001 <smaile3001@gmail.com>
2023-07-26 16:06:45 +0300

    favorite = [true, false]

--- controllers/contact-controlles.js
+++ controllers/contact-controlles.js
@@ -4,13 +4,16 @@ import { Contact } from '../models/contact.js';
 
 const getAll = async (req, res) => {
   const { _id: owner } = req.user;
-  const { page = 1, limit = 10 } = req.query;
+  const { page = 1, limit = 10, favorite = [true, false] } = req.query;
   const skip = (page - 1) * limit;
-  const result = await Contact.find({ owner }, '-createdAt -updatedAt', {
-    skip,
-    limit,
-  }).populate('owner', 'name email');
-
+  const result = await Contact.find(
+    { owner, favorite },
+    '-createdAt -updatedAt',
+    {
+      skip,
+      limit,
+    }
+  ).populate('owner', 'name email');
   res.json(result);
 };
 
